025135ebcb8943ece22873425bb167357cb98342
Probal Ranjan Paul <probal.ranjan1198@gmail.com>
2022-03-01 21:43:49 +0600

    search error handling

--- js/app.js
+++ js/app.js
@@ -8,11 +8,22 @@ const searchMobile = () => {
     // ---- clear previous search
     searchField.value = ""
 
-    // ---- load API data for results
-    const url = `https://openapi.programming-hero.com/api/phones?search=${searchText}`
-    fetch(url)
-        .then(res => res.json())
-        .then(data => displaySearchResult(data.data))
+
+    // ---- error messages
+    if (searchText === "") {
+        const emptySearch = document.getElementById("empty-search")
+        emptySearch.classList = "alert alert-danger w-50 mt-4 mx-auto d-block"
+    }
+
+    else {
+        document.getElementById("empty-search").classList = "d-none"
+
+        // ---- load API data for results
+        const url = `https://openapi.programming-hero.com/api/phones?search=${searchText}`
+        fetch(url)
+            .then(res => res.json())
+            .then(data => displaySearchResult(data.data))
+    }
 }
 
 /* ---- Display all Results ---- */
@@ -48,6 +59,12 @@ const displaySearchResult = mobiles => {
         // --- append results on div
         searchResult.appendChild(createDiv)
     })
+
+    // ---- load more button
+    const loadBotton = document.getElementById("load-btn")
+    loadBotton.classList = "btn btn-primary mt-4 mx-auto d-block"
+    document.body.appendChild(loadBotton)
+
 }
 
 /* ---- Load Details of API Data ---- */
@@ -75,16 +92,28 @@ const displayMobileDetails = mobile => {
 
     // ---- create HTML for show details
     createDiv.innerHTML = `
-        <div class="card mb-3 w-50 mx-auto py-3">
+        <div class="card mb-3 mx-auto py-3">
             <div class="row g-0">
-                <div class="col-md-6 d-flex align-items-center">
-                    <img src="${mobile.image}" class="rounded mx-auto d-block" alt="...">
+                <div class="col-md-6 col-12 d-flex align-items-center">
+                    <img src="${mobile.image}" class="rounded mx-auto d-block w-50" alt="...">
                 </div>
-                <div class="col-md-6 d-flex align-items-center">
+                <div class="col-md-6 col-12 d-flex align-items-center">
                     <div class="card-body">
-                        <h5 class="card-title pb-1 border-bottom border-2">${mobile.name}</h5>
-                        <p class="card-title pb-1 border-bottom border-2">${mobile.releaseDate}</p>
-                        <p class="card-text pb-1 border-bottom border-2">${mobile.brand}</p>
+                        <h5 class="card-title pb-1 border-bottom border-2 fw-bold">${mobile.name}</h5>
+                        <p class="card-text pb-1 border-bottom border-2"><span class="fw-bold">Brand:</span>
+                        ${mobile.brand}</p>
+                        <p class="card-title pb-1 border-bottom border-2"><span class="fw-bold">Release
+                            Date:</span> ${mobile.releaseDate}</p>
+                        <p class="card-text pb-1 border-bottom border-2"><span class="fw-bold">Display:</span>
+                        ${mobile.mainFeatures.displaySize}</p>
+                        <p class="card-text pb-1 border-bottom border-2"><span class="fw-bold">Storage:</span>
+                        ${mobile.mainFeatures.storage}</p>
+                        <p class="card-text pb-1 border-bottom border-2"><span class="fw-bold">Chip:</span>
+                        ${mobile.mainFeatures.chipSet}</p>
+                        <p class="card-text pb-1 border-bottom border-2"><span class="fw-bold">Memory:</span>
+                        ${mobile.mainFeatures.memory}</p>
+                        <p class="card-text pb-1 border-bottom border-2"><span class="fw-bold">Sensors:</span>
+                        ${mobile.mainFeatures.sensors.forEach(sensor => sensor[0])}</p>
                     </div>
                 </div>
             </div>
