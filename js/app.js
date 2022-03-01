c2911a265f04650a81800f13e0cb5cbe10b258c3
Probal Ranjan Paul <probal.ranjan1198@gmail.com>
2022-03-01 06:04:06 +0600

    showing details result

--- js/app.js
+++ js/app.js
@@ -1,5 +1,6 @@
 /* ---- Search Input Field ---- */
 const searchMobile = () => {
+
     // ---- get input values
     const searchField = document.getElementById("search-field")
     const searchText = searchField.value
@@ -7,7 +8,7 @@ const searchMobile = () => {
     // ---- clear previous search
     searchField.value = ""
 
-    // ---- load API data
+    // ---- load API data for results
     const url = `https://openapi.programming-hero.com/api/phones?search=${searchText}`
     fetch(url)
         .then(res => res.json())
@@ -16,6 +17,7 @@ const searchMobile = () => {
 
 /* ---- Display all Results ---- */
 const displaySearchResult = mobiles => {
+
     // ---- get search result section
     const searchResult = document.getElementById("serach-result")
 
@@ -24,6 +26,8 @@ const displaySearchResult = mobiles => {
 
     // ---- get each result
     mobiles.forEach(mobile => {
+
+        // ---- div create
         const createDiv = document.createElement('div')
         createDiv.classList.add("col")
 
@@ -40,16 +44,53 @@ const displaySearchResult = mobiles => {
                 </div>
             </div>
         `
+
+        // --- append results on div
         searchResult.appendChild(createDiv)
     })
 }
 
+/* ---- Load Details of API Data ---- */
 const mobileDetails = mobileSlug => {
-    const url = `https://openapi.programming-hero.com/api/phone/${mobileSlug}`
-    console.log(url);
 
+    // ---- load API data for details
+    const url = `https://openapi.programming-hero.com/api/phone/${mobileSlug}`
     fetch(url)
         .then(res => res.json())
-        .then(data => console.log(data.data))
+        .then(data => displayMobileDetails(data.data))
+}
 
+/* ---- Display Mobile Details ---- */
+const displayMobileDetails = mobile => {
+
+    // ---- get search details section
+    const mobileDetails = document.getElementById("mobile-details")
+
+    // ---- clear previous result of details
+    mobileDetails.textContent = ""
+
+    // ---- div create
+    const createDiv = document.createElement('div')
+    createDiv.classList.add("col")
+
+    // ---- create HTML for show details
+    createDiv.innerHTML = `
+        <div class="card mb-3 w-50 mx-auto py-3">
+            <div class="row g-0">
+                <div class="col-md-6 d-flex align-items-center">
+                    <img src="${mobile.image}" class="rounded mx-auto d-block" alt="...">
+                </div>
+                <div class="col-md-6 d-flex align-items-center">
+                    <div class="card-body">
+                        <h5 class="card-title pb-1 border-bottom border-2">${mobile.name}</h5>
+                        <p class="card-title pb-1 border-bottom border-2">${mobile.releaseDate}</p>
+                        <p class="card-text pb-1 border-bottom border-2">${mobile.brand}</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    `
+
+    // --- append details on div
+    mobileDetails.appendChild(createDiv)
 }
